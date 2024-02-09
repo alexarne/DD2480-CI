33b11b730f4f1a6fdad5afdfab34bce13a104d96
Juan Lavagnini <juanlavagnini@gmail.com>
2024-02-09 17:52:27 +0100

    Feat: positive test for sanitizer

---
+++ ci/src/test/java/com/group21/ci/TextSanitizerTest.java
@@ -0,0 +1,20 @@
+package com.group21.ci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TextSanitizerTest {
+
+    /*
+     * Positive Case, for a valid input, no changes are expected
+     */
+
+    @Test
+    public void testSanitize_PositiveCase() {
+        String input = "This is a test";
+        String expected = "This is a test";
+        assertEquals(expected, TextSanitizer.sanitize(input));
+    }
+
+}
