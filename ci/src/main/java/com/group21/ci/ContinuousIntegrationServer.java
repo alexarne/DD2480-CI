63f31e1dec632b7b45a353be1e2a61ded6af4377
Alex Gunnarsson <alexgunnarsson@live.se>
2024-02-10 20:31:42 +0100

    feat: add history browsing

--- ci/src/main/java/com/group21/ci/ContinuousIntegrationServer.java
+++ ci/src/main/java/com/group21/ci/ContinuousIntegrationServer.java
@@ -3,11 +3,14 @@ package com.group21.ci;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
- 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 import org.eclipse.jetty.server.Server;
-import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
@@ -25,27 +28,61 @@ public class ContinuousIntegrationServer extends AbstractHandler
         throws IOException, ServletException
     {
         System.out.println(target);
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
         switch (request.getMethod()) {
             case "POST":
                 
                 break;
             case "GET":
+                PrintWriter writer = response.getWriter();
+                String htmlRespone = "<html>";
+                if (target.equals("/")) {
+                    System.out.println("Printing build history");
+                    htmlRespone += "<h1>Previous Builds:</h1>";
+                    File file = new File(Config.DIRECTORY_BUILD_HISTORY);
+                    String[] directories = file.list(new FilenameFilter() {
+                        @Override
+                        public boolean accept(File current, String name) {
+                            return new File(current, name).isDirectory();
+                        }
+                    });
+                    if (directories != null) {
+                        for (String directory : directories) {
+                            htmlRespone += "<a href=\"/" + directory + "\"><h3>" + directory + "</h3></a>";
+                        }
+                    }
+
+                } else if (target.matches("^/[0-9]+$")) {
+                    String buildId = target.substring(1);
+                    System.out.println("Printing build history for " + buildId);
+                    htmlRespone += "<h1>Build " + buildId + "</h1>";
+                    Scanner branchReader = new Scanner(new File(Config.DIRECTORY_BUILD_HISTORY + buildId + "/" + Config.BUILD_BRANCH_FILENAME));
+                    Scanner SHAReader = new Scanner(new File(Config.DIRECTORY_BUILD_HISTORY + buildId + "/" + Config.BUILD_IDENTIFIER_FILENAME));
+                    Scanner logReader = new Scanner(new File(Config.DIRECTORY_BUILD_HISTORY + buildId + "/" + Config.BUILD_LOG_FILENAME));
+                    htmlRespone += "<h2>Branch: " + branchReader.nextLine() + "</h2>";
+                    htmlRespone += "<h2>SHA: " + SHAReader.nextLine() + "</h2>";
+                    htmlRespone += "<h2>Build log:</h2>";
+                    while (logReader.hasNextLine()) {
+                        htmlRespone += logReader.nextLine() + "<br>";
+                    }
+                    branchReader.close();
+                    SHAReader.close();
+                    logReader.close();
+                } else {
+                    if (!target.equals("/favicon.ico")) 
+                        System.out.println("Unknown get request for " + target);
+                    break;
+                }
                 
+                htmlRespone += "</html>";
+                writer.println(htmlRespone);
                 break;
         
             default:
                 break;
         }
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
-
-        response.getWriter().println("CI job done");
     }
  
     // used to start the CI server in command line
@@ -56,7 +93,7 @@ public class ContinuousIntegrationServer extends AbstractHandler
         String SHA = "qowdpinqwdoin";
         String branch = "master";
         RepositoryTester repositoryTester = new RepositoryTester(owner, repositoryName, SHA, branch);
-        repositoryTester.runTests();
+        // repositoryTester.runTests();
         Server server = new Server(Config.PORT);
         server.setHandler(new ContinuousIntegrationServer()); 
         server.start();
