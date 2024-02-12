3fab2109c210f046507e81c14feefae171c3fd2b
Alex Gunnarsson <alexgunnarsson@live.se>
2024-02-12 23:01:04 +0100

    fix: checkout the sha instead of branch

--- ci/src/main/java/com/group21/ci/RepositoryTester.java
+++ ci/src/main/java/com/group21/ci/RepositoryTester.java
@@ -81,7 +81,7 @@ public class RepositoryTester {
                 process.redirectError(Redirect.appendTo(logFile));
                 process.start().waitFor();
                 process.directory(new File(dir));
-                process.command("git", "checkout", branch);
+                process.command("git", "checkout", SHA);
                 process.start().waitFor();
                 process.command("bash",  "test.sh");
                 exitCode = process.start().waitFor();
