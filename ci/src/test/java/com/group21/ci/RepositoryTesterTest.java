746a5c3a699c69aee5688931d31a037dd9adae91
TRICOT-Hugo <tricot@kth.se>
2024-02-13 17:33:38 +0100

    test: remove old negative test (see details) and add new test by checking files created
    The old test would loop infinitely after the push as it needs to test itself

--- ci/src/test/java/com/group21/ci/RepositoryTesterTest.java
+++ ci/src/test/java/com/group21/ci/RepositoryTesterTest.java
@@ -7,24 +7,6 @@ import org.junit.*;
 public class RepositoryTesterTest {
 
     /**
-     * Positive test for compilation. The RepositoryTester's runTest method
-     * clones and compiles the project on a known, working commit, and returns
-     * exit code 0 (all went well)
-     */
-    @Test
-    public void testCompilationOnWorkingCommit() {
-        RepositoryInfo repo = new RepositoryInfo("main",
-                                        "baeaaec85b5a3b480d842e1fa8a5253405f054b3",
-                                        "https://github.com/alexarne/DD2480-CI.git",
-                                           "alexarne",
-                                            "DD2480-CI");
-        RepositoryTester tester = new RepositoryTester(repo);
-        int exitCode = tester.runTests();
-        assertTrue(exitCode == 0);
-    }
-
-
-    /**
      * Negative test for compilation. The RepositoryTester's runTest method
      * clones the project but cannot find the commit, whose id is wrong
      */
@@ -39,5 +21,8 @@ public class RepositoryTesterTest {
         int exitCode = tester.runTests();
         assertTrue(exitCode != 0);
     }
+
+    // Note: a similar positive test was made but it ended up looping by checking itself. It was removed
+    // in favor of CompulingTest.testCreatedFilesFound()
 
 }
