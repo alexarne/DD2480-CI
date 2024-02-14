128e01d24d9f0c06d0f0803abefb244cf1ceb2f6
haaker1 <haaker.anne@gmail.com>
2024-02-14 13:18:27 +0100

    doc: document static constants

--- ci/src/main/java/com/group21/ci/Config.java
+++ ci/src/main/java/com/group21/ci/Config.java
@@ -60,11 +60,28 @@ public class Config {
         .ignoreIfMissing()
         .load()
         .get("FAILURE_DESCRIPTION", null);
-
+    /**
+     * The network port used by the server.
+     */
     public static final int PORT = 8021;
+    /**
+     * Path to directory for saving cloned repositories.
+     */
     public static final String DIRECTORY_REPOSITORIES = "./repos/";
+    /**
+     * Path to directory for saving build history.
+     */
     public static final String DIRECTORY_BUILD_HISTORY = "./build_history/";
+    /**
+     * File for saving output from compilation and testing.
+     */
     public static final String BUILD_LOG_FILENAME = "build_log.txt";
+    /**
+     * File to save the commit id (SHA) identifying the commit the repository was built from.
+     */
     public static final String BUILD_IDENTIFIER_FILENAME = "commit_identifier.txt";
+    /**
+     * File to save the name of the branch that was built and tested.
+     */
     public static final String BUILD_BRANCH_FILENAME = "branch.txt";
 }
