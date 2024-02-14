4fb02a5d68b1d464d68685da1b7d8037709f3ed1
haaker1 <haaker.anne@gmail.com>
2024-02-14 13:33:38 +0100

    doc: add javadoc files

---
+++ ci/src/main/java/com/group21/ci/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes to create a continouos integration server
+ * that builds and tests a GitHub repository. It is triggered by a 
+ * webhook on push events and sends commit status as notification of 
+ * the status of the build.
+ */
+package com.group21.ci;
