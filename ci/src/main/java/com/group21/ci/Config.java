4fcd7f00a3a35273ace644547ef4daa3c2e66983
haaker1 <haaker.anne@gmail.com>
2024-02-14 12:21:40 +0100

    doc: document some constants

--- ci/src/main/java/com/group21/ci/Config.java
+++ ci/src/main/java/com/group21/ci/Config.java
@@ -6,42 +6,61 @@ import io.github.cdimascio.dotenv.Dotenv;
  * A class holding configurations for the server.
  */
 public class Config {
+    /**
+     * A token to verify setting of commit statuses.
+     */
     public static final String GITHUB_TOKEN = Dotenv.configure()
         .directory("..")
         .filename("config.env")
         .ignoreIfMissing()
         .load()
         .get("GITHUB_TOKEN", "");
+    /**
+     * URL to page showing build history.
+     */
     public static final String HISTORY_URL = Dotenv.configure()
         .directory("..")
         .filename("config.env")
         .ignoreIfMissing()
         .load()
         .get("HISTORY_URL", "http://localhost:8021/");
+    /**
+     * Custom description for success commit status.
+     */
     public static String CUSTOM_SUCCESS_DESCRIPTION = Dotenv.configure()
         .directory("..")
         .filename("config.env")
         .ignoreIfMissing()
         .load()
         .get("SUCCESS_DESCRIPTION", null);
+    /**
+     * Custom description for pending commit status.
+     */
     public static String CUSTOM_PENDING_DESCRIPTION = Dotenv.configure()
         .directory("..")
         .filename("config.env")
         .ignoreIfMissing()
         .load()
         .get("PENDING_DESCRIPTION", null);
+    /**
+     * Custom description for error commit status.
+     */
     public static String CUSTOM_ERROR_DESCRIPTION = Dotenv.configure()
         .directory("..")
         .filename("config.env")
         .ignoreIfMissing()
         .load()
         .get("ERROR_DESCRIPTION", null);
+    /**
+     * Custom description for failure commit status.
+     */
     public static String CUSTOM_FAILURE_DESCRIPTION = Dotenv.configure()
         .directory("..")
         .filename("config.env")
         .ignoreIfMissing()
         .load()
         .get("FAILURE_DESCRIPTION", null);
+
     public static final int PORT = 8021;
     public static final String DIRECTORY_REPOSITORIES = "./repos/";
     public static final String DIRECTORY_BUILD_HISTORY = "./build_history/";
